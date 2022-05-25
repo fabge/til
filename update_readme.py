faaba4c8c171dc9fa2afe3282d607156ab55db6c
fabge <geiger.fabian@outlook.com>
2022-05-25 23:57:07 +0200

    Update

--- update_readme.py
+++ update_readme.py
@@ -2,7 +2,11 @@ from pathlib import Path
 import re
 
 root = Path('.')
-readme = []
+readme = ['''---
+css:
+  custom: >-
+    @media (prefers-color-scheme: dark) {html {filter: invert(1);}}
+---''']
 for folder in sorted(root.iterdir()):
     if folder.is_dir() and not folder.as_posix().startswith('.'):
         readme.append(f'## {folder}')
@@ -18,25 +22,25 @@ with open('README.md', 'w') as f:
 
 
 
-index = ['''
-<head>
-@media (prefers-color-scheme: dark) {
-    html {
-        filter: invert(1);
-    }
-}
-</head>
-''']
-for folder in sorted(root.iterdir()):
-    if folder.is_dir() and not folder.as_posix().startswith('.'):
-        index.append(f'<h2>{folder}</h2>')
-        for file in folder.iterdir():
-            with open(file, 'r') as f:
-                md = f.read()
-            title = re.search('(?<=# ).*(?=\n)', md).group()
-            index.append(f'<li>[{title}]({file})</li>')
-        index.append('<br>')
-    index.append('<br>')
+# index = ['''
+# <head>
+# @media (prefers-color-scheme: dark) {
+#     html {
+#         filter: invert(1);
+#     }
+# }
+# </head>
+# ''']
+# for folder in sorted(root.iterdir()):
+#     if folder.is_dir() and not folder.as_posix().startswith('.'):
+#         index.append(f'<h2>{folder}</h2>')
+#         for file in folder.iterdir():
+#             with open(file, 'r') as f:
+#                 md = f.read()
+#             title = re.search('(?<=# ).*(?=\n)', md).group()
+#             index.append(f'<li>[{title}]({file})</li>')
+#         index.append('<br>')
+#     index.append('<br>')
 
-with open('index.html', 'w') as f:
-    f.write("<br>".join(index))
+# with open('index.html', 'w') as f:
+#     f.write("<br>".join(index))
