e2737661a2684f8a31107ded4cd96c568e235379
fabge <geiger.fabian@outlook.com>
2022-05-26 00:23:20 +0200

    Update

--- update_readme.py
+++ update_readme.py
@@ -2,10 +2,7 @@ from pathlib import Path
 import re
 
 root = Path('.')
-readme = ['''# TIL
-
-Things I've learned, collected in [fabge/til](https://github.com/fabge/til).
-''']
+readme = ['Things I\'ve learned, collected in [fabge/til](https://github.com/fabge/til).']
 for folder in sorted(root.iterdir()):
     if folder.is_dir() and not folder.as_posix().startswith(('.', 'assets')):
         readme.append(f'## {folder}')
