48a1306948e718755f6e4ef59dc9c1cc1a2f73ac
fabge <geiger.fabian@outlook.com>
2022-05-25 23:37:15 +0200

    Update

--- update_readme.py
+++ update_readme.py
@@ -2,7 +2,13 @@ from pathlib import Path
 import re
 
 root = Path('.')
-index = []
+index = ['''
+@media (prefers-color-scheme: dark) {
+    html {
+        filter: invert(1);
+    }
+}
+''']
 for folder in sorted(root.iterdir()):
     if folder.is_dir() and not folder.as_posix().startswith('.'):
         index.append(f'## {folder}')
