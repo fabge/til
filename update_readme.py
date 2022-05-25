ca38120cc7ead3b0aa766a0dec1039b5f1d367dc
fabge <geiger.fabian@outlook.com>
2022-05-25 23:24:18 +0200

    Update

--- update_readme.py
+++ update_readme.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 root = Path('.')
 index = []
@@ -6,7 +7,10 @@ for folder in sorted(root.iterdir()):
     if folder.is_dir() and not folder.as_posix().startswith('.'):
         index.append(f'## {folder}')
         for file in folder.iterdir():
-            index.append(f'* [{file.stem}]({file})')
+            with open(file, 'r') as f:
+                md = f.read()
+            title = re.search('(?<=# ).*(?=\n)', md).group()
+            index.append(f'* [{title}]({file})')
         index.append('\n')
 
 with open('README.md', 'w') as f:
