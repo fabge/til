d0f8088bfaad2717898e85697be4891b067dddb4
fabge <geiger.fabian@outlook.com>
2022-05-25 22:55:25 +0200

    Update

--- update_readme.py
+++ update_readme.py
@@ -6,7 +6,7 @@ for folder in root.iterdir():
     if folder.is_dir() and not folder.as_posix().startswith('.'):
         index.append(f'## {folder}')
         for file in folder.iterdir():
-            index.append(f'* [{file.name}]({file})')
+            index.append(f'* [{file.stem}]({file})')
         index.append('\n')
 
 with open('README.md', 'w') as f:
