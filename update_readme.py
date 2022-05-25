2feef13d6980480fa19550927ee4aa06858f0126
fabge <geiger.fabian@outlook.com>
2022-05-25 22:53:51 +0200

    Update

--- update_readme.py
+++ update_readme.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 root = Path('.')
-index = ['# TIL']
+index = []
 for folder in root.iterdir():
-    if folder.is_dir():
+    if folder.is_dir() and not folder.as_posix().startswith('.'):
         index.append(f'## {folder}')
         for file in folder.iterdir():
             index.append(f'* [{file.name}]({file})')
